4fc80f66727fc51b52f2faad5764bbd145557c81
mehdi-cse-bd <mehdi.cse.bd@gmail.com>
2024-02-16 08:05:00 -0500

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+//Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/blog")
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
